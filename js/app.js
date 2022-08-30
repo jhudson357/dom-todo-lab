fe730bc9c817db3e222af3472fa17c253a399039
James Hudson <jmh357@comcast.net>
2022-08-30 17:31:54 -0500

    add reset button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const inputElement = document.getElementById('input')
 const buttonElement = document.getElementById('submit-button')
 const ulElement = document.getElementById('todo-list')
+const resetButtonElement = document.getElementById('reset-button')
+
 
 buttonElement.addEventListener('click', handleClick)
+resetButtonElement.addEventListener('click', handleResetClick)
+
 
 function handleClick(evt) {
   // create new <li> element
@@ -14,9 +18,22 @@ function handleClick(evt) {
   //don't allow empty items to be added to list
   if(inputElement.value !== '') {
     // append the <li> (with the text included) to the <ul> in the HTML
-    document.querySelector('ul').appendChild(li)
+    ulElement.appendChild(li)
   } 
   
   // clear the input field to an empty string
   inputElement.value = ''
 }
+
+function handleResetClick (evt) {
+  // clear input field
+  inputElement.value = ''
+
+  // remove all items from the to-do list
+  document.getElementById('todo-list').innerHTML = ''
+}
+
+// RESET BUTTON - alternate way to add using JS
+// const resetButton = document.createElement('button')
+// resetButton.innerHTML = 'Reset'
+// document.body.appendChild(resetButton)
