70cf252e92f11781b87c2d6d4e750faf4d4a7d9c
James Hudson <jmh357@comcast.net>
2022-08-30 17:00:23 -0500

    prevent empty items being added to list

--- js/app.js
+++ js/app.js
@@ -7,12 +7,15 @@ buttonElement.addEventListener('click', handleClick)
 function handleClick(evt) {
   // create new <li> element
   const li = document.createElement('li')
-  
+
   // access the text from the inputElement & store it in the <li>
   li.textContent = inputElement.value
   
-  // append the <li> (with the text included) to the <ul> in the HTML
-  document.querySelector('ul').appendChild(li)
+  //don't allow empty items to be added to list
+  if(inputElement.value !== '') {
+    // append the <li> (with the text included) to the <ul> in the HTML
+    document.querySelector('ul').appendChild(li)
+  } 
   
   // clear the input field to an empty string
   inputElement.value = ''
